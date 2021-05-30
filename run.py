65a52d941d6ba00914d225a4e70312ba290732a7
Felipe Andres Souza Alarcon <fsouza@it-irl-xv0515.lan>
2021-05-30 10:09:14 +0100

    changed port/ip for heroku config

--- run.py
+++ run.py
@@ -368,7 +368,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True
+        # host=os.environ.get("IP"),
+        # port=int(os.environ.get("PORT")),
+        # debug=True
     )
